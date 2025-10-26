674f9fa083d99588263ca4a8ddd15dc9c66585f2
burcin ozkan <hburcinozkan@gmail.com>
2025-10-26 20:55:20 +0300

    feat(main): initialize application entrypoint with router and DB

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	_ "github.com/joho/godotenv/autoload"
+	"video-platform-backend/internal/db"
+	"video-platform-backend/internal/models"
+	"video-platform-backend/internal/routes"
+	"video-platform-backend/internal/storage"
+)
+
+func main() {
+	db.ConnectDB()
+	db.DB.AutoMigrate(&models.Task{})
+	storage.InitS3()
+
+	r := routes.SetupRouter()
+	r.Run(":8080") // ✅ dikkat, port 8080
+}
